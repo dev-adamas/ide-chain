6668b0a09f823b517b90dea01a015bb54ad7193a
dev-adamas <real.dev.adamas@gmail.com>
2026-08-13 12:49:51 +0100

    added animation on scroll

--- jquery/main.js
+++ jquery/main.js
@@ -1,7 +1,11 @@
 //
-$('#testimonialWrapper');
+// $('#testimonialWrapper');
 
-$('#testimonalWrapper .card').each((el)=>{
-  console.log(el);
+// $('#testimonalWrapper .card').each((el)=>{
+//   console.log(el);
   
-})
+// })
+
+let container = $('#testimonialWrapper');
+
+console.log(container);
